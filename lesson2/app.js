2f2f0924afbcd3737a7eeaa787ca7409d603089b
Vladimir Paranitsa <vladimir.paranitsa@dev-pro.net>
2017-01-18 23:23:03 +0200

    added listeners to delete method

--- lesson2/app.js
+++ lesson2/app.js
@@ -20,19 +20,57 @@ app.get('/', function (req, res) {
     res.send('<html><body><h1>My web app http API! Version ' + apiVersion + '</h1></body></html>');
 });
 
-app.get
-
 app.post('/post', function (req, res) {
     res.send('<html><body><h1>Post body' + req + '</h1></body></html>');
 });
 
+app.delete('/api/:version/posts|users/*', (req, res) => {
+    if (req.path.indexOf(apiVersion) > -1) {
+        let folderPath = path.join(__dirname, req.path.replace(apiVersion + '/', ''));
+
+        fs.stat(folderPath, (error, stats) => {
+            if (error) {
+                console.log(error);
+                return;
+            }
+
+            if (stats.isDirectory) {
+                fs.readdir(folderPath, (readError, files) => {
+                    files.map((file, index) => {
+                        fs.unlink(path.join(folderPath, file), (unlinkError) => {
+                            if (unlinkError) {
+                                console.log(unlinkError);
+                            }
+
+                            if (files.length - 1 === index) {
+                                fs.rmdir(folderPath, (removeError) => {
+                                    if (removeError) {
+                                        console.log(removeError);
+                                        return;
+                                    }
+
+                                    res.end(JSON.stringify([{"status": "success"}]));
+                                });
+                            }
+                        });
+                    });
+                });
+            } else {
+                res.end([{"status": "fail"}]);
+            }
+        });
+
+    } else {
+        res.end([{"status": "fail"}]);
+    }
+});
 
 app.all('/test/', function (req, res) {
     res.json('{ test: "Hello test" }');
 });
 
 
-app.all('/api/:apiVersion/*', function (req, res) {
+app.get('/api/:apiVersion/*', function (req, res) {
     let fileName = req.method.toLowerCase() + '.json';
 
     var filePath = req.path + fileName;
@@ -72,4 +110,8 @@ app.all('/api/:apiVersion/*', function (req, res) {
         });
     });
 
+});
+
+app.all('*', (req, res) => {
+    res.end('Something went wrong');
 });
